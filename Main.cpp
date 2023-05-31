e029d83970ce97d0d12fd63d02a801967af811ac
dalius.budrys <dalius.budrys@unity3d.com>
2023-05-31 20:37:43 +0300

    Cube3D Cube

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,13 @@
 //#include "EngineApplication.hpp"
 //#include "HelloTriangleApplication.hpp"
 //#include "TexturingApplication.hpp"
-#include "Setting3DApplication.hpp"
+//#include "Setting3DApplication.hpp"
+#include "3DRenderingApplication.hpp"
 
 int main(int argc, char* argv[])
 {
-    Setting3DApplication application{ "D3D11 Engine" };
+    Rendering3DApplication application{ "D3D11 Engine" };
+    //Setting3DApplication application{ "D3D11 Engine" };
+
     application.Run();
 }
