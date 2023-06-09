7f38ea7f516115e2707ecc3f49203b867c837cdf
dalius.budrys <dalius.budrys@unity3d.com>
2023-06-09 19:18:37 +0300

    Revert "Don't remove libs"
    This reverts commit d697eae851103ff1befd54168ed1e9619a458a0b.

--- Dependencies/Engine/include/WindowsXpPipesSimulation.hpp
+++ Dependencies/Engine/include/WindowsXpPipesSimulation.hpp
@@ -54,8 +54,7 @@ public:
     void Update(const float deltaTime) override;
     void Render(ID3D11DeviceContext* deviceContext, ID3D11Buffer* perObjectConstantBuffer) override;
 
-    Int3 GetNextCell(const Int3& currentPosition) const;
-    Int3 GetPreviousCell(const Int3& currentPosition, const Direction direction) const;
+    Int3 GetNextCell();
     Direction GetNextDirection(const std::vector<Direction>& availableDirections) const;
     std::vector<Direction> GetAvailableDirections() const;
     DirectX::XMFLOAT3 GetCellWorldPosition(const Int3& cellPosition) const;
