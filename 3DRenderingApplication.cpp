70378efd0562c5ac86cc38ccf82945c9177b96c2
dalius.budrys <dalius.budrys@unity3d.com>
2023-07-09 20:27:21 +0300

    Adjust UI text

--- 3DRenderingApplication.cpp
+++ 3DRenderingApplication.cpp
@@ -346,13 +346,8 @@ void Rendering3DApplication::Render()
     static float f = 0.0f;
     static int counter = 0;
 
-    ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-        counter++;
-    ImGui::SameLine();
-    ImGui::Text("counter = %d", counter);
+    ImGui::Begin("Debug Info");                
+    ImGui::Text(std::to_string(1 / _deltaTime).c_str());
     ImGui::End();
 
     ImGui::Render();
