ca70b11b45c5b5d760cbf4c17ca6ba14bb887145
dalius.budrys <dalius.budrys@unity3d.com>
2023-07-28 00:41:46 +0300

    Fix unresolved externals

--- Headers/IntVectors.hpp
+++ Headers/IntVectors.hpp
@@ -3,9 +3,9 @@ struct Int3 {
     int y;
     int z;
 
-    Int3() {};
+    Int3() { x = 0; y = 0; z = 0; };
     Int3(const int x, const int y, const int z);
-    ~Int3();
+    ~Int3() {}
 
     Int3 operator+(const Int3& other) const;
     Int3 operator-(const Int3& other) const;
